c02b6e332ea20a8e4450dd5b59bd314657e75162
CruCk <microdharma@gmail.com>
2016-02-12 09:44:56 +0530

    test

--- rainbowapp/models.py
+++ rainbowapp/models.py
@@ -5,7 +5,6 @@ class CustomerDetails(models.Model):
 	firstname = models.CharField(max_length=50,blank=False,default=None)
 	lastname = models.CharField(max_length=50,blank=False,default=None)
 	username = models.CharField(max_length=20, unique=True)
-
 	email = models.EmailField(unique=True)
 	password = models.CharField(max_length=20)
 	mobilenumber = models.CharField(max_length=15, unique = True)
