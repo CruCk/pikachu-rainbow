9b496edf32ed31ddb2d72c3ed46fc0b6a896c33c
CruCk <microdharma@gmail.com>
2016-02-13 00:05:59 +0530

    made some minorchanges

--- rainbowapp/urls.py
+++ rainbowapp/urls.py
@@ -2,13 +2,13 @@ from django.conf.urls import  patterns, include, url
 from . import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.home, name='home'),
-	url(r'^pikachu/$', views.home, name='home'),
+	url(r'^$', views.index, name='index'),
+	url(r'^pikachu/$', views.index, name='index'),
 	url(r'^login/$', views.login, name='login'),
     url(r'^login_check/$', views.login_check, name='login_check'),
     url(r'^registration/$', views.registration, name='registration'),
     url(r'^registration_check/$', views.registration_check, name='registration_check'),
-    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^home/$', views.home, name='home'),
     url(r'^logout/$', views.logout, name='logout'),
     url(r'^forgotpwd/$', views.forgotpwd, name='forgotpwd'),
 	)
--- rainbowapp/views.py
+++ rainbowapp/views.py
@@ -8,19 +8,17 @@ from forms import CustomerDetailsForm
 from django.template import RequestContext
 
 # Create your views here.
-def home(request):
+def index(request):
 	args = {}
 	args.update(csrf(request))
 	return render_to_response('index.html',args)
 def login(request):
     args = {}
     args.update(csrf(request))
-    return render_to_response('login.html',args)
+    return render_to_response('index.html',args)
 def login_check(request):
 	username = request.POST.get('username','')
 	password = request.POST.get('password','')
-	print username
-	print password
 	try:
 		user = CustomerDetails.objects.get(email=username)
 	except CustomerDetails.DoesNotExist:
@@ -28,7 +26,7 @@ def login_check(request):
 	if user is not None:
 		if user.password == password:
 			request.session['name'] = username
-			return render_to_response('profile.html',{'username' : user.username})
+			return HttpResponseRedirect('/pikachu/home')
 		else:
 			html = "<html><body>Password Error</body></html>"
 			return HttpResponse(html)
@@ -38,26 +36,24 @@ def login_check(request):
 def registration(request):
     args = {}
     args.update(csrf(request))
-    return render_to_response('registration.html',args)
+    return render_to_response('index.html',args)
 def registration_check(request):
     form = CustomerDetailsForm()
-    
-    print "*********8"
-    # print request
     if request.POST:
         form = CustomerDetailsForm(request.POST)
         name = request.POST.get('mobilenum')
         print name
-
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/pikachu/login/')
         else:           
             return render_to_response('mytemplate.html',{'form':form})
     return render_to_response('registration.html',{'form':form}, context_instance=RequestContext(request))
-def profile(request):
-	return render_to_response('profile.html')
+def home(request):
+		return render_to_response('home.html',{'username' : request.session['name']})
 def logout(request):
-	return render_to_response('home.html')
+	args = {}
+	args.update(csrf(request))
+	return render_to_response('index.html')
 def forgotpwd(request):
 	return render_to_response('forgotdetails.html')
